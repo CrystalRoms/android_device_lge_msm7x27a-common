8de86cb396e8cac5ff90ae2bd6627004f1d60215
cr3pt <cr3pt@o2.pl>
2014-01-28 21:53:04 +0100

    Revert "msm7x27a-common: Add New GPS HAL"
    This reverts commit 776403d8d3a1a1031679fcadc0e9e14385702413.

--- msm7x27a-common.mk
+++ msm7x27a-common.mk
@@ -1,5 +1,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
 DEVICE_PACKAGE_OVERLAYS += device/lge/msm7x27a-common/overlay
 
 PRODUCT_COPY_FILES += \
@@ -92,8 +95,7 @@ PRODUCT_COPY_FILES += \
 
 # Gps hal
 PRODUCT_PACKAGES += \
-    gps.msm7x27a \
-    gps.conf
+    gps.msm7x27a
 
 PRODUCT_PACKAGES += \
     camera.msm7x27a
