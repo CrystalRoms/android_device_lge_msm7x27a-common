2482fef66929bfbdd9d0bbee296b5c717ed1d87a
cr3pt <cr3pt@o2.pl>
2014-01-30 10:32:25 +0100

    Readd FM 	modified:   Android.mk

--- libaudio/Android.mk
+++ libaudio/Android.mk
@@ -19,7 +19,7 @@ ifeq ($(BOARD_HAVE_BLUETOOTH_BLUEZ),true)
   LOCAL_CFLAGS += -DWITH_A2DP
 endif
 
-ifeq ($(BOARD_HAVE_QCOM_FM),true)
+ifeq ($(BOARD_HAVE_QCOM_MR1_FM),true)
   LOCAL_CFLAGS += -DWITH_QCOM_FM
   LOCAL_CFLAGS += -DQCOM_FM_ENABLED
 endif
@@ -99,6 +99,10 @@ endif
 
 ifeq ($(BOARD_HAVE_BLUETOOTH_BLUEZ),true)
   LOCAL_CFLAGS += -DWITH_A2DP
+endif
+
+ifeq ($(BOARD_HAVE_QCOM_MR1_FM),true)
+  LOCAL_CFLAGS += -DQCOM_FM_ENABLED
 endif
 
 LOCAL_C_INCLUDES := hardware/libhardware_legacy/audio
